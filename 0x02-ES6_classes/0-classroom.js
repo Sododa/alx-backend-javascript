2a68a2348697f9f9fe842e3bf51658314f77a865
Sododa <sophieododa@gmail.com>
2024-07-25 13:20:00 +0300

    classroom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 export default class ClassRoom {
   /**
-   Implement a class named ClassRoom*/
+   * Implements a class named ClassRoom
+   * @param {Number} maxStudentsSize - maximum number of students in class
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
