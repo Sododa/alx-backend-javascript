0f78d6642aa08481876444f39a5fe6209cafbeee
Sododa <sophieododa@gmail.com>
2024-09-19 17:29:12 +0300

    chai

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,27 +1,14 @@
-/**
- * Performs a mathematical operation on two rounded numbers
- * @param {string} type - The type of operation to perform (SUM, SUBTRACT,
- * or DIVIDE)
- * @param {number} a - The first number to be rounded and used in the
- */
-function calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-
-  switch (type) {
-    case 'SUM':
-      return roundedA + roundedB;
-    case 'SUBTRACT':
-      return roundedA - roundedB;
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
-
-    default:
-      throw new Error(`Invalid type: ${type}`);
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
   }
-}
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return 0;
+};
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,20 +1,12 @@
-/**
- * Simulates an API call to get a payment token
- * @param {boolean} success - Whether the API call is successful or not
- * @returns {Promise} A Promise that resolves with an object containing
- * a "data" property if success is true. Otherwise, the Promise does nothing.
- */
-function getPaymentTokenFromAPI(success) {
-  // eslint-disable-next-line no-unused-vars
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ data: 'Successful response from the API' });
-    } else {
-      // We are not doing anything if success is false, 
-      // but we can reject the promise if desired
-      // reject('Error: API call failed');
-    }
-  });
-}
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
-module.exports = getPaymentTokenFromAPI;
+describe('getPaymentTokenFromAPI', () => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      });
+  });
+});
